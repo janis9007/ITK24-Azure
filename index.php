6c8eca4776928c7e96b8a299f93eb63228bf7320
janis9007 <janisjogi90@gmail.com>
2025-03-14 15:03:44 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
         <h1><Meie projekt ITK24 grupiga</h1>
           <p>See leht arvestab meie muutustega</p>
           <img src"https://www.tuur.ee/wp-content/uploads/2017/02/01-vaade-koolihoonele-kehtna-thumb.jpg">
-          <body>
+        </body>
       </html>
 
   
