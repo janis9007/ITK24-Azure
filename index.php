23a51fd14a8c575b90585c1304925637b3036ad2
janis9007 <janisjogi90@gmail.com>
2025-03-14 14:52:50 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
   <head>
     <title>Itk24 Azure/title>
     </head>
+      <body>
+        <h1><Meie projekt ITK24 grupiga</h1>
+          <p>See leht arvestab meie muutustega</p>
+          <body>
       </html>
 
   
