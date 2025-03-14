321a34cb2b1ed8154a7cd935a7d8b7bb45afe726
janis9007 <janisjogi90@gmail.com>
2025-03-14 15:07:13 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <title>Itk24 Azure/<title>
     </head>
       <body>
-        <h1><Meie projekt ITK24 grupiga</h1>
+        <h1>Meie projekt ITK24 grupiga</h1>
           <p>See leht arvestab meie muutustega</p>
           <img src="https://www.tuur.ee/wp-content/uploads/2017/02/01-vaade-koolihoonele-kehtna-thumb.jpg">
         </body>
